d7e182d1e1c92d882feef48306888740e46c6c39
Marcel Wiget <mwiget@gmail.com>
2024-05-17 11:39:03 +0200

    adding support for untagged (vlan .0)

--- appstack/sriov-interfaces.tf
+++ appstack/sriov-interfaces.tf
@@ -2,7 +2,7 @@ resource "restapi_object" "network_interfaces" {
   for_each = var.sriov_vlans
   id_attribute = "metadata/name"
   path         = "/config/namespaces/system/network_interfaces"
-  data         = jsonencode({
+  data         = split(".", each.value)[1] > 0 ? jsonencode({
     "metadata": {
       "name": format("%s-%s-%s", var.f5xc_cluster_name, split(".", each.value)[0], split(".", each.value)[1]),
       "description": var.description,
@@ -13,6 +13,20 @@ resource "restapi_object" "network_interfaces" {
         "l2sriov_interface": {
           "device": split(".", each.value)[0]
           "vlan_id": split(".", each.value)[1]
+        }
+      }
+    }
+  }) : jsonencode({
+    "metadata": {
+      "name": format("%s-%s-%s", var.f5xc_cluster_name, split(".", each.value)[0], split(".", each.value)[1]),
+      "description": var.description,
+      "disable": false
+    },
+    "spec": {
+      "layer2_interface": {
+        "l2sriov_interface": {
+          "device": split(".", each.value)[0]
+          "untagged": {}
         }
       }
     }
